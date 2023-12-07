fffb78b7720c0c3ca555201b98e257b95c6fc87e
ajackwere <austinewere59@gmail.com>
2023-12-07 12:42:53 +0300

    added a patch for job view

--- server/jobseekerapp/views.py
+++ server/jobseekerapp/views.py
@@ -17,6 +17,23 @@ class JobViewSet(viewsets.ModelViewSet):
     queryset = Job.objects.all()
     serializer_class = JobSerializer
 
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(status=204)
+
+    def perform_destroy(self, instance):
+        instance.delete()
+
+    def partial_update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(serializer.data)
+
+    def perform_update(self, serializer):
+        serializer.save()
 class JobApplicationViewSet(viewsets.ModelViewSet):
     queryset = JobApplication.objects.all()
     serializer_class = JobApplicationSerializer
