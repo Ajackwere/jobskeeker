f43303dddc197c53750f512d97578f758d9ad7f7
ajackwere <austinewere59@gmail.com>
2023-12-07 12:46:06 +0300

    added patch request  for job seeker and employer

--- server/jobseekerapp/views.py
+++ server/jobseekerapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.response import Response
 from .models import JobSeeker, Employer, Job, JobApplication
 from .serializers import JobSeekerSerializer, EmployerSerializer, JobSerializer, JobApplicationSerializer
 
@@ -9,10 +10,29 @@ class JobSeekerViewSet(viewsets.ModelViewSet):
     queryset = JobSeeker.objects.all()
     serializer_class = JobSeekerSerializer
 
+    def partial_update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(serializer.data)
+
+    def perform_update(self, serializer):
+        serializer.save()
+
 class EmployerViewSet(viewsets.ModelViewSet):
     queryset = Employer.objects.all()
     serializer_class = EmployerSerializer
 
+    def partial_update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(serializer.data)
+
+    def perform_update(self, serializer):
+        serializer.save()
 class JobViewSet(viewsets.ModelViewSet):
     queryset = Job.objects.all()
     serializer_class = JobSerializer
