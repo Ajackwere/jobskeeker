e602b81f48d54516f9f37a56e3e3cdc5f4a507be
ajackwere <austinewere59@gmail.com>
2023-12-10 16:52:29 +0300

    updated databases

--- server/server/settings.py
+++ server/server/settings.py
@@ -72,7 +72,7 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
+        'HOST': '',
         'PORT': os.getenv('DB_PORT'),
     }
 }
