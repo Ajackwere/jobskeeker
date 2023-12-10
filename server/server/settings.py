9896c13f6b016eb282da4540fbc65caaf2446539
ajackwere <austinewere59@gmail.com>
2023-12-10 13:34:07 +0300

    Created a db backup and connected to postgres

--- server/server/settings.py
+++ server/server/settings.py
@@ -66,7 +66,11 @@ WSGI_APPLICATION = 'server.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'jobseeker',
+        'USER': 'were',
+        'PASSWORD': 'were',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
