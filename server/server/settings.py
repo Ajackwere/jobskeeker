72851bdd9330395f559fe554d59954a055fa9114
ajackwere <austinewere59@gmail.com>
2023-12-10 16:55:59 +0300

    env variable updated

--- server/server/settings.py
+++ server/server/settings.py
@@ -68,12 +68,12 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': '',
-        'PORT': os.getenv('DB_PORT'),
+        'ENGINE':'django.db.backends.postgresql',
+        'NAME':os.getenv('DB_NAME'),
+        'USER':os.getenv('DB_USER'),
+        'PASSWORD':os.getenv('DB_PASSWORD'),
+        'HOST':os.getenv('DB_HOST'),
+        'PORT':os.getenv('DB_PORT'),
     }
 }
 
