a165b59de97d2d76b700645fe9e4949ca671ebe4
ajackwere <austinewere59@gmail.com>
2023-12-16 17:56:01 +0300

    added config for secret key

--- server/server/settings.py
+++ server/server/settings.py
@@ -1,12 +1,12 @@
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', default='')
+SECRET_KEY = config('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
