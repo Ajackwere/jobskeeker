c65a54a237782dc510dc79c9c1ab4fd69cdbbb9d
ajackwere <austinewere59@gmail.com>
2024-01-29 13:23:11 +0300

    secret key updated

--- server/server/settings.py
+++ server/server/settings.py
@@ -7,7 +7,7 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
