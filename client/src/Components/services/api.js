e1c7cd30e87a34484b557326a0439418b775f09e
ajackwere <austinewere59@gmail.com>
2024-01-31 15:42:13 +0300

    update base_url service

--- client/src/Components/services/api.js
+++ client/src/Components/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://127.0.0.1:8000/';
+const API_BASE_URL = 'https://jobseekerhub.onrender.com/';
 
 const api = axios.create({
     baseURL: API_BASE_URL,
